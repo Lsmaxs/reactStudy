05dcbc68fe9cf9aed8fc847c3f5485b086e4936e
Lsmaxs <shuan93431631@qq.com>
2017-07-05 11:40:56 +0800

    增加少量配置

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
+import Library from './library';
+import React from  "react";
+import {render} from  "react-dom";
 
-document.write("hello world")
+class App extends React.Component{
+    constructor(...prop){
+        super(...prop);
+    }
+    render(){
+        <div className="header">react learn</div>
+    }
+}
+
+render(<App />,document.getElementById("app"));
+
+if (module.hot) {
+    module.hot.accept('./library', function() {
+        console.log('Accepting the updated library module!');
+        Library.log();
+        document.write("dfasdf11112O111")
+    })
+}
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,11 @@
  * Created by Administrator on 2017/7/4.
  */
 const path = require('path');
-const HtmlWebpackG = require('html-webpack-plugin');
+const HtmlWebpackP = require('html-webpack-plugin');
+const webpack  = require('webpack');
+const OpenBrowserP = require('open-browser-webpack-plugin');
+const ExtractTextP = require('extract-text-webpack-plugin')
+
 
 function resolve(dir) {
     return path.resolve(__dirname,dir);
@@ -13,17 +17,47 @@ module.exports = {
     },
     output: {
         path: resolve("dist"),
+        publicPath: './',
         filename: '[name].[hash].js'
     },
     module:{
         rules:[
-
+            {
+                test:/\.(js|jsx)$/,
+                exclude:/node_modules/,
+                use:"babel-loader"
+            }
         ]
     },
+    resolve: {
+        extensions: ['.js','.jsx','.scss','.css'],
+        alias:{
+            "src":resolve('src')
+        }
+    },
     devtool: 'cheap-module-source-map',
+    devServer: {
+        proxy:{},
+        publicPath:"/",
+        contentBase:resolve('dist'),
+        host: process.env.HOST,
+        compress: true,
+        port:8011,
+        hot: true,
+        noInfo: true,
+        inline: true,
+        overlay: {
+            errors: true,
+            warnings: true,
+        },
+    },
     plugins:[
-        new HtmlWebpackG({
+        new HtmlWebpackP({
             title:"webpack dome"
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new OpenBrowserP({
+            url:"http://localhost:8011"
         })
     ]
 }
